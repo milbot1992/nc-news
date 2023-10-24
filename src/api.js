64addbe678d0572b09093894c1bfad864fef818e
Millie Ellis <ellismillie92@gmail.com>
2023-10-24 11:28:37 +0100

    Ability to vote on an article

--- src/api.js
+++ src/api.js
@@ -2,13 +2,24 @@ import axios from 'axios'
 
 const newsAPI = axios.create({ baseURL : 'https://news-api-905c.onrender.com/api'})
 
-export const getArticles = () => {
-    return newsAPI.get('/articles', {params: {
-        sort_by: 'created_at'
-    }})
+export const getArticles = (topic) => {
+    const query = {
+        params: {
+            sort_by: 'created_at',
+        },
+    };
+    
+    if (topic) {
+        query.params.topic = topic;
+    }
+    
+    return newsAPI.get('/articles', query)
     .then((res) => {
-        return res.data.articles
+        return res.data.articles;
     })
+    .catch((error) => {
+        console.error(error);
+    });
 }
 
 export const getArticleById = (article_id) =>{
@@ -23,4 +34,8 @@ export const getCommentsById = (article_id) =>{
     .then(({data}) =>{
         return data.comments;
     })
+}
+
+export const patchLikes = (value, article_id) => {
+    return newsAPI.patch(`/articles/${article_id}`, {inc_votes: value})
 }
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -4,16 +4,26 @@ import { getArticleById } from "../api";
 import { getCommentsById } from "../api";
 import CommentCard from "./CommentCard";
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import Voter from "./Voter";
+import { patchLikes } from "../api";
+import {Link} from 'react-router-dom'
+import '../Single-article.css'
+import Loading from "./Loading";
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
 
 export default function SingleArticle() {
     const { article_id } = useParams();
     const [article, setArticle] = useState({});
     const [comments, setComments] = useState([])
-
+    const [loading, setLoading] = useState(true);
+    const [userLikes, setUserLikes] = useState(0);
+    const [isErr, setIsErr] = useState(false);
+    
     useEffect(() => {
         getArticleById(article_id)
         .then((article) => {
             setArticle(article);
+            setLoading(false)
         })
         .catch((err) => {
             console.log(err);
@@ -35,35 +45,59 @@ export default function SingleArticle() {
         timeAgo = formatDistanceToNow(new Date(created_at), { addSuffix: true });
     }
 
+    const updateAPILikes = (value) => {
+        patchLikes(value, article_id)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            setIsErr(true);
+            setUserVotes(0);
+        });
+    }
+
+    if (loading) return <Loading />;
+
     return (
         <>
+        <Link to="/news" className='back-button'>&laquo;</Link>
         <div className = 'single-article-info'>
             <p className="author">{author}</p>
             <p className="time-ago">{timeAgo}</p>
         </div>
         <h2>{title}</h2>
-        <p></p>
+        <p className = 'image-banner'>{topic}&nbsp;&nbsp;</p>
         <img className = 'single-article-image'
             src={article_img_url}
             alt={`a photo to depict the article: ${title}`}
             id="medium-image"
         />
         <p className = 'single-article-body'>{body}</p>
-        <div className = 'single-article-votes'>
-            <h3>Votes: {votes} </h3>
-            <h3>Comments: {comment_count}</h3>
-            <h3>{topic}</h3>
+        <div className = 'single-article-banner'>
+            <Voter 
+                type={'likes'} 
+                votes={votes} 
+                updateAPILikes={updateAPILikes} 
+                setUserLikes = {setUserLikes} 
+                userLikes = {userLikes} 
+                isErr = {isErr}/>
+            <h3 className="comment-count">
+                <a href="#first-comment-card">
+                    <ChatRoundedIcon />
+                    {comment_count}
+                </a>
+            </h3>
         </div>
 
-        <ul className = 'comments-list'>
-            {comments.map(({ comment_id, body, author, votes, created_at }) => (
+        <ul className='comments-list'>
+            {comments.map(({ comment_id, body, author, votes, created_at }, index) => (
                 <Fragment key={comment_id}>
-                <li>
+                <li id={index === 0 ? 'first-comment-card' : ''}>
                     <CommentCard
-                    body={body}
-                    author={author}
-                    votes={votes}
-                    created_at={created_at}
+                        body={body}
+                        author={author}
+                        votes={votes}
+                        created_at={created_at}
                     />
                 </li>
                 </Fragment>
